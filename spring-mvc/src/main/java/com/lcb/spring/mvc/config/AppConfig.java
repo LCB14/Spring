de741dbc3f07c362479ce65ba32699cf07469036
changbao.li <changbao.li@bianlifeng.com>
2019-08-16 01:17:17 +0800

    2019年08月16日01:17:15

---
+++ spring-mvc/src/main/java/com/lcb/spring/mvc/config/AppConfig.java
@@ -0,0 +1,13 @@
+package com.lcb.spring.mvc.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author changbao.li
+ * @since 15 八月 2019
+ */
+@Configuration
+@ComponentScan("com")
+public class AppConfig {
+}
---
+++ spring-mvc/src/main/java/com/lcb/spring/mvc/controller/TestController.java
@@ -0,0 +1,22 @@
+package com.lcb.spring.mvc.controller;
+
+import com.lcb.spring.mvc.entity.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * @author changbao.li
+ * @since 15 八月 2019
+ */
+@Controller
+@RequestMapping("/test")
+public class TestController {
+
+    @RequestMapping("/getUser.do")
+    @ResponseBody
+    public Object getUser(User user){
+        System.out.println("进来了");
+        return user;
+    }
+}
