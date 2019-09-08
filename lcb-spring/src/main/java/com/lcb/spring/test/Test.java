d943f41d8a24523fa95f5a0e9f38e21adf539de8
changbao.li <changbao.li@bianlifeng.com>
2019-09-08 09:46:10 +0800

    2019年09月08日09:46:09

--- lcb-spring/src/main/java/com/lcb/spring/test/Test.java
+++ lcb-spring/src/main/java/com/lcb/spring/test/Test.java
@@ -2,6 +2,7 @@ package com.lcb.spring.test;
 
 import com.lcb.spring.appconfig.AppConfig;
 import com.lcb.spring.bean.Student;
+import com.lcb.spring.other.People;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import java.util.Arrays;
@@ -17,12 +18,14 @@ public class Test {
         AnnotationConfigApplicationContext annotationConfigApplicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
 
-        Student student = annotationConfigApplicationContext.getBean(Student.class);
-
-        System.out.println(student.toString());
+//        Student student = annotationConfigApplicationContext.getBean(Student.class);
+//        System.out.println(student.toString());
 
         // 获取所有已经注册bean的名称
 //        String[] beanDefinitionNames = annotationConfigApplicationContext.getBeanDefinitionNames();
 //        System.out.println(Arrays.toString(beanDefinitionNames));
+
+        People people = (People)annotationConfigApplicationContext.getBean("people");
+        people.sOut();
     }
 }
