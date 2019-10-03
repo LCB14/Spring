795fe5941c19f7e89f6a3dde72f1702d697492d3
changbaol <changbaol@tujia.com>
2019-10-03 17:00:29 +0800

    2019年10月03日17:00:27

--- lcb-spring/src/main/java/com/lcb/spring/test/Test.java
+++ lcb-spring/src/main/java/com/lcb/spring/test/Test.java
@@ -18,14 +18,14 @@ public class Test {
     public static void main(String[] args) {
         AnnotationConfigApplicationContext annotationConfigApplicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-//        Student student = annotationConfigApplicationContext.getBean(Student.class);
-//        System.out.println(student.toString());
+        Student student = annotationConfigApplicationContext.getBean(Student.class);
+        System.out.println(student.toString());
 
         // 获取所有已经注册bean的名称
 //        String[] beanDefinitionNames = annotationConfigApplicationContext.getBeanDefinitionNames();
 //        System.out.println(Arrays.toString(beanDefinitionNames));
-
-        People people = (People)annotationConfigApplicationContext.getBean("people");
-        people.sOut();
+//
+//        People people = (People)annotationConfigApplicationContext.getBean("people");
+//        people.sOut();
     }
 }
