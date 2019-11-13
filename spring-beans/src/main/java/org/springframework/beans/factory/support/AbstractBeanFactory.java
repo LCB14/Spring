2e0e0823197d71293dddcb0c9dba47ee507f8156
changbaol <changbaol@tujia.com>
2019-11-13 15:38:29 +0800

    2019年11月13日15:38:27

--- spring-beans/src/main/java/org/springframework/beans/factory/support/AbstractBeanFactory.java
+++ spring-beans/src/main/java/org/springframework/beans/factory/support/AbstractBeanFactory.java
@@ -278,7 +278,16 @@ public abstract class AbstractBeanFactory extends FactoryBeanRegistrySupport imp
     protected <T> T doGetBean(final String name, @Nullable final Class<T> requiredType,
                               @Nullable final Object[] args, boolean typeCheckOnly) throws BeansException {
 
-        // 处理bean name带有‘&'操作符的情况（主要针对实现FactoryBean这种特殊的bean场景)
+         /*
+         * 通过 name 获取 beanName。这里不使用 name 直接作为 beanName 有两点原因：
+         *
+         * 1. name 可能会以 & 字符开头，表明调用者想获取 FactoryBean 本身，而非 FactoryBean
+         *    getObject()返回的bean实例。在 BeanFactory 中，FactoryBean 的实现类和其他的 bean 存储
+         *    方式是一致的，即 <beanName, bean>，beanName 中是没有 & 这个字符的。所以我们需要
+         *    将 name 的首字符 & 移除，这样才能从缓存里取到 FactoryBean 实例。
+         * 2. 若 name 是一个别名，则应将别名转换为具体的实例名，也就是 beanName。
+         *
+         * */
         final String beanName = transformedBeanName(name);
         Object bean;
 
